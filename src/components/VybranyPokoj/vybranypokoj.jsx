79aac4ef6f62fcfa423cee97e90e6535c650040c
Daniela Pech <randakova.daniela@gmail.com>
2025-05-24 11:25:53 +0200

    uprava formulare

--- src/components/VybranyPokoj/vybranypokoj.jsx
+++ src/components/VybranyPokoj/vybranypokoj.jsx
@@ -17,29 +17,51 @@ export const VybranyPokoj = () => {
           <form>
             <div className="form-fields">
               <label htmlFor="field1" className="field-label">
-                Field 1:
+                Od:
               </label>
-              <input id="field1" className="field-input" type="text" />
+              <input id="field1" className="field-input" type="date" />
 
               <label htmlFor="field2" className="field-label">
-                Field 2:
+                Do:
               </label>
-              <input id="field2" className="field-input" type="text" />
+              <input id="field2" className="field-input" type="date" />
+
+              <label htmlFor="field3" className="field-label">
+                Počet osob:
+              </label>
+              <input id="field3" className="field-input" type="text" />
 
               <label htmlFor="select" className="field-label">
-                Select:
+                Stravování:
               </label>
               <select id="select" className="field-input">
-                <option>Option 1</option>
+                <option>Žádné</option>
                 <option>Option 2</option>
                 <option>Option 3</option>
                 <option>Option 4</option>
               </select>
 
               <label htmlFor="check1" className="field-label">
-                Checkbox 1:
+                Domácí mazlíček:
               </label>
               <input id="check1" className="field-input" type="checkbox" />
+              <label htmlFor="check2" className="field-label">
+                Přistýlka pro dítě:
+              </label>
+              <input id="check2" className="field-input" type="checkbox" />
+              <label htmlFor="check3" className="field-label">
+                Bezbariérový přístup:
+              </label>
+              <input id="check3" className="field-input" type="checkbox" />
+
+              <label htmlFor="field4" className="field-label">
+                E-mail:
+              </label>
+              <input id="field4" className="field-input" type="email" />
+              <label htmlFor="field5" className="field-label">
+                Telefon:
+              </label>
+              <input id="field5" className="field-input" type="tel" />
             </div>
             <button className="wide">Submit</button>
           </form>
